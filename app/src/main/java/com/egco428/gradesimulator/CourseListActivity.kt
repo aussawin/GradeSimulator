72265cd8027936824ac5972474fd0d84662375db
Winner <notovel2@gmail.com>
2017-10-31 22:21:35 +0700

    initial CourseList

---
+++ app/src/main/java/com/egco428/gradesimulator/CourseListActivity.kt
@@ -0,0 +1,13 @@
+package com.egco428.gradesimulator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CourseListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course_list)
+
+    }
+}
