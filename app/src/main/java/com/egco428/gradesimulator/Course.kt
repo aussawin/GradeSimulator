4e419198721f7196dfdaa066b9f908b93aebc580
aussawin <Aussawin@Aussawins-MacBook-Air.local>
2017-11-02 00:23:22 +0700

    edit some conflicts

---
+++ app/src/main/java/com/egco428/gradesimulator/Course.kt
@@ -0,0 +1,9 @@
+package com.egco428.gradesimulator
+
+/**
+ * Created by Aussawin on 11/2/2017 AD.
+ */
+class Course (val courseNo: String = "",
+              val name: String = "",
+              val cat: String = "") {
+}
