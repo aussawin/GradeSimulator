38d82741f659918214c11dda08b6854739fdf3d9
aussawin <Aussawin@Aussawins-MacBook-Air.local>
2017-11-02 00:27:44 +0700

    solve conflicts

--- app/src/main/java/com/egco428/gradesimulator/Course.kt
+++ app/src/main/java/com/egco428/gradesimulator/Course.kt
@@ -3,14 +3,8 @@ package com.egco428.gradesimulator
 /**
  * Created by Aussawin on 11/2/2017 AD.
  */
-<<<<<<< HEAD
-class Course (val courseNo: String = "",
-              val name: String = "",
-              val cat: String = "") {
-}
-=======
+
 data class Course(val courseNo: String = "",
                   val name: String = "",
                   val cat: String = "") {
 }
->>>>>>> origin/develop
--- app/src/main/java/com/egco428/gradesimulator/CourseListActivity.kt
+++ app/src/main/java/com/egco428/gradesimulator/CourseListActivity.kt
@@ -9,9 +9,6 @@ import android.view.*
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_course_list.*
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.course_row.view.*
 
 class CourseListActivity : AppCompatActivity() {
 
